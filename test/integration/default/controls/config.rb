6534a2ffab5ce233146193cd304fb40cf686cc3b
Eric Veiras Galisson <eric@sietch-tabr.com>
2018-12-10 18:05:17 +0100

    more Inspec tests

---
+++ test/integration/default/controls/config.rb
@@ -0,0 +1,9 @@
+title 'Verify haproxy configuration'
+
+describe file('/etc/haproxy/haproxy.cfg') do
+  it { should be_file }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should cmp '0644' }
+end
+

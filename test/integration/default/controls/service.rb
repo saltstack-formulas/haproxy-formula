22a1b6bb8440c95c0361ba76a639010364f5656a
Nicolas Rodriguez <nicoladmin@free.fr>
2019-10-16 00:29:29 +0200

    test(inspec): add tests on HAProxy ports

--- test/integration/default/controls/service.rb
+++ test/integration/default/controls/service.rb
@@ -8,4 +8,32 @@ control 'HAProxy service' do
     it { should be_enabled }
     it { should be_running }
   end
+
+  describe port(8888) do
+    it { should be_listening }
+    its('processes') { should include 'haproxy' }
+    its('protocols') { should include 'tcp' }
+    its('addresses') { should include '0.0.0.0' }
+  end
+
+  describe port(8998) do
+    it { should be_listening }
+    its('processes') { should include 'haproxy' }
+    its('protocols') { should include 'tcp' }
+    its('addresses') { should include '0.0.0.0' }
+  end
+
+  describe port(6379) do
+    it { should be_listening }
+    its('processes') { should include 'haproxy' }
+    its('protocols') { should include 'tcp' }
+    its('addresses') { should include '0.0.0.0' }
+  end
+
+  describe port(80) do
+    it { should be_listening }
+    its('processes') { should include 'haproxy' }
+    its('protocols') { should include 'tcp' }
+    its('addresses') { should include '0.0.0.0' }
+  end
 end
